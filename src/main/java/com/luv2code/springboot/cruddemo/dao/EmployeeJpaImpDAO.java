3227898479ebdbbd0c6ecde36a2712f2805a3216
Osama Salih <osamasalih.ca@gmail.com>
2025-02-20 10:52:47 +0300

    Employee entity and dao

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeJpaImpDAO.java
@@ -0,0 +1,27 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeJpaImpDAO implements EmployeeDAO{
+
+    EntityManager entityManager;
+
+    @Autowired
+    public EmployeeJpaImpDAO(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+        return theQuery.getResultList();
+    }
+}
