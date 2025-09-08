3fc0bd4a0629516b373968e3c4fe2d85cab83b83
Osama Salih <osamasalih.ca@gmail.com>
2025-09-08 19:11:26 +0300

    Employee Service

---
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.luv2code.springboot.cruddemo.service;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+}
