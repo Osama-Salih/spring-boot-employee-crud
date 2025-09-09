6a79ef930385c48efb436e7b339e890bbe70b976
Osama Salih <osamasalih.ca@gmail.com>
2025-09-09 10:11:44 +0300

    Service implementaion

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -4,4 +4,7 @@ import java.util.List;
 
 public interface EmployeeService {
     List<Employee> findAll();
+    Employee findById(Integer id);
+    Employee save(Employee employee);
+    void deleteById(Integer id);
 }
--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,8 @@ import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -18,5 +20,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return this.employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(Integer id) {
+        return this.employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return this.employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(Integer id) {
+        this.employeeDAO.deleteById(id);
     }
 }
