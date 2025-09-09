4f72f77e2fc1bd24a39cba5408e6724f13ed6df9
Osama Salih <osamasalih.ca@gmail.com>
2025-09-09 15:14:11 +0300

    Employee crud

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,26 +1,92 @@
 package com.luv2code.springboot.cruddemo.rest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
     private final EmployeeService employeeService;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public EmployeeRestController(EmployeeService employeeService) {
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper) {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return this.employeeService.findAll();
+    }
+
+    @GetMapping("employees/{employeeId}")
+    public Employee findOne(@PathVariable int employeeId) {
+        Employee employee = this.employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee not found!");
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee save(@RequestBody Employee employee) {
+        employee.setId(0);
+        return this.employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee update(@RequestBody Employee employee) {
+        return this.employeeService.save(employee);
+    }
+
+    @PatchMapping("/employees/{employeeId}")
+    public Employee updateEmployeeUsingPatch(@PathVariable int employeeId, @RequestBody Map<String, Object> employeeData) {
+        Employee employee = this.employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee not found!");
+        }
+
+        if (employeeData.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee updatedEmployee = apply(employee, employeeData);
+        return this.employeeService.save(updatedEmployee);
+    }
+
+    private Employee apply(Employee employee, Map<String, Object> employeeData) {
+        // Convert employee object to a JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(employee, ObjectNode.class);
+
+        // Convert updatedEmpNode object to a JSON object node
+        ObjectNode updatedEmpNode = objectMapper.convertValue(employeeData, ObjectNode.class);
+
+        // Merge the patch updates into the employee node
+        employeeNode.setAll(updatedEmpNode);
+        return objectMapper.convertValue(employeeNode, Employee.class);
+    }
+
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String delete(@PathVariable int employeeId) {
+        Employee employee = this.employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee not found!");
+        }
+
+        this.employeeService.deleteById(employeeId);
+        return "Deleted employee with id - " + employeeId;
     }
 }
