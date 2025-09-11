41dfa64699b9613613753f34ee765fb3c7d8b34e
Osama Salih <osamasalih.ca@gmail.com>
2025-09-11 10:48:29 +0300

    spring-data-rest

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.cruddemo.service;
-import com.luv2code.springboot.cruddemo.entity.Employee;
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> findAll();
-    Employee findById(Integer id);
-    Employee save(Employee employee);
-    void deleteById(Integer id);
-}
+//package com.luv2code.springboot.cruddemo.service;
+//import com.luv2code.springboot.cruddemo.entity.Employee;
+//import java.util.List;
+//
+//public interface EmployeeService {
+//    List<Employee> findAll();
+//    Employee findById(Integer id);
+//    Employee save(Employee employee);
+//    void deleteById(Integer id);
+//}
--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,49 +1,49 @@
-package com.luv2code.springboot.cruddemo.service;
-import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
-import com.luv2code.springboot.cruddemo.entity.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-
-    private final EmployeeRepository employeeRepository;
-
-    @Autowired
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
-
-    @Override
-    public List<Employee> findAll() {
-        return this.employeeRepository.findAll();
-    }
-
-    @Override
-    public Employee findById(Integer id) {
-        Optional<Employee> result = this.employeeRepository.findById(id);
-
-        Employee employee = null;
-        if (result.isPresent()) {
-            employee = result.get();
-        } else {
-            throw new RuntimeException("Employee not found! id - " + id);
-        }
-
-        return employee;
-    }
-
-
-    @Override
-    public Employee save(Employee employee) {
-        return this.employeeRepository.save(employee);
-    }
-
-    @Override
-    public void deleteById(Integer id) {
-        this.employeeRepository.deleteById(id);
-    }
-}
+//package com.luv2code.springboot.cruddemo.service;
+//import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+//import com.luv2code.springboot.cruddemo.entity.Employee;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class EmployeeServiceImpl implements EmployeeService {
+//
+//    private final EmployeeRepository employeeRepository;
+//
+//    @Autowired
+//    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+//
+//    @Override
+//    public List<Employee> findAll() {
+//        return this.employeeRepository.findAll();
+//    }
+//
+//    @Override
+//    public Employee findById(Integer id) {
+//        Optional<Employee> result = this.employeeRepository.findById(id);
+//
+//        Employee employee = null;
+//        if (result.isPresent()) {
+//            employee = result.get();
+//        } else {
+//            throw new RuntimeException("Employee not found! id - " + id);
+//        }
+//
+//        return employee;
+//    }
+//
+//
+//    @Override
+//    public Employee save(Employee employee) {
+//        return this.employeeRepository.save(employee);
+//    }
+//
+//    @Override
+//    public void deleteById(Integer id) {
+//        this.employeeRepository.deleteById(id);
+//    }
+//}
